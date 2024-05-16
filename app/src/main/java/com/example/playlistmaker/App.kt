cc763aab3a0f26a763c75ba46ec0b8b06fa0ddff
Nadezhda <nadyachery@gmail.com>
2024-05-16 18:50:15 +0300

    Добавление модуля для работы с репозиториями.

--- app/src/main/java/com/example/playlistmaker/App.kt
+++ app/src/main/java/com/example/playlistmaker/App.kt
@@ -3,6 +3,7 @@ package com.example.playlistmaker
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.playlistmaker.di.networkModule
+import com.example.playlistmaker.di.repositoryModule
 import com.example.playlistmaker.util.Creator
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -16,7 +17,8 @@ class App : Application() {
         startKoin {
             androidContext(this@App)
             modules(
-                networkModule
+                networkModule,
+                repositoryModule
             )
         }
 
